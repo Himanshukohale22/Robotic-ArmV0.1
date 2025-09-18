1cdb71109c37c8e65e0b7b06bb66d077ef9a9b69
himanshukohale <kohalehimanshu22@gmail.com>
2025-09-18 16:03:59 +0530

    Inversekindematics.c is main file now

--- src/InverseKinemarics.c
+++ src/InverseKinemarics.c
@@ -24,17 +24,24 @@ typedef struct {
     int x, y, z;
 } Point3D;
 
+float degree_angle = 180/PI;
+
+
 ///////////////////////////////////////////////////////////////////////////////////
 //inverse kinematics
 
 int inverse_kinematics(double xe, double ye, double ze, double theta_e) {
+    
+
+    theta_0 = atan2(ze, xe);
+
     double xw = xe - (L3 * cos(theta_e));
     double yw = ye - (L3 * sin(theta_e));
-    double zw = ze;
+    double zw = ze ;
+    //float theta_e = 0;
 
     double r = sqrt(xw*xw + yw*yw);
 
-    theta_0 = atan2(ze, xe);
 
     double a = (L1*L1 + r*r - L2*L2) / (2.0 * L1 * r);
     double b = (L1*L1 + L2*L2 - r*r) / (2.0 * L1 * L2);
@@ -48,18 +55,19 @@ int inverse_kinematics(double xe, double ye, double ze, double theta_e) {
     // theta_1 = atan2(zw, sqrt(xw*xw + yw*yw)) + acos(a);
     theta_1 = atan2(yw, xw) + acos(a);
 
-    theta_2 = (PI/2 + acos(-b));   // pi/2+acosb
+    theta_2 = -(PI - acos(b));   // pi/2+acosb
 
-    theta_3 = PI/2 + theta_e - theta_1 - theta_2;
+    theta_3 =  theta_e - theta_1 - theta_2;
 
     return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
 //movement in z direction keeping x-10 constant 
-void zigzag_motion_1D(Point3D start, Point3D end, FILE *fptr) {
+void zigzag_motion_1D(Point3D start, Point3D end,FILE *fptr, FILE *fptr_agnlerad, FILE *fptr_angledegree) {
     int dir = 1; // 1 = forward in Y, -1 = backward
     int x = start.x;
+    float theta_e = 0;
 
     for (int z = start.z; z <= end.z; z += STEP_z) {
         if (dir == 1) {
@@ -68,13 +76,16 @@ void zigzag_motion_1D(Point3D start, Point3D end, FILE *fptr) {
                 inverse_kinematics(x,y,z,theta_e);
                 printf("%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
                 fprintf(fptr, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
-
+                fprintf(fptr_agnlerad, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, PI+theta_2, (PI/2)+theta_3);
+                fprintf(fptr_angledegree, "%d %d %d %f %f %f %f \n", x, y, z, (theta_0)*degree_angle, (theta_1)*degree_angle, (PI+theta_2)*degree_angle, ((PI/2)+theta_3)*degree_angle);
             }
         } else {
             for (int y = end.y; y >= start.y; y -= STEP_Y) {
                 inverse_kinematics(x,y,z,theta_e);
                 printf("%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
                 fprintf(fptr, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
+                fprintf(fptr_agnlerad, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, PI+theta_2, theta_3);
+                fprintf(fptr_angledegree, "%d %d %d %f %f %f %f \n", x, y, z, (theta_0)*degree_angle, (theta_1)*degree_angle, (PI+theta_2)*degree_angle, (theta_3)*degree_angle);
             }
         }
         dir *= -1; // flip direction each row
@@ -82,10 +93,10 @@ void zigzag_motion_1D(Point3D start, Point3D end, FILE *fptr) {
 }
 
 //movement in z direction keeping with variable x
-void zigzag_motion_2D(Point3D start, Point3D end, FILE *fptr) {
+void zigzag_motion_2D(Point3D start, Point3D end,FILE *fptr ,FILE *fptr_agnlerad, FILE *fptr_angledegree) {
     int dir = 1; // 1 = forward in Y, -1 = backward
     int x = start.x;
-
+    float theta_e = 0;
     for (int y = start.y; y <= end.y; y += STEP_Y) {
         if (dir == 1) {
 
@@ -93,12 +104,16 @@ void zigzag_motion_2D(Point3D start, Point3D end, FILE *fptr) {
                 inverse_kinematics(x,y,z,theta_e);
                 printf("%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
                 fprintf(fptr, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
+                fprintf(fptr_agnlerad, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, PI+theta_2, theta_3);
+                fprintf(fptr_angledegree, "%d %d %d %f %f %f %f \n", x, y, z, (theta_0)*degree_angle, (theta_1)*degree_angle, (PI+theta_2)*degree_angle, (theta_3)*degree_angle);
             }
         } else {
             for (int z = end.z; z >= start.z; z -= STEP_z) {
                 inverse_kinematics(x,y,z,theta_e);
                 printf("%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
                 fprintf(fptr, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, theta_2, theta_3);
+                fprintf(fptr_agnlerad, "%d %d %d %f %f %f %f \n", x, y, z, theta_0, theta_1, PI+theta_2, theta_3);
+                fprintf(fptr_angledegree, "%d %d %d %f %f %f %f \n", x, y, z, (theta_0)*degree_angle, (theta_1)*degree_angle, (PI+theta_2)*degree_angle, (theta_3)*degree_angle);
             }
         }
         dir *= -1; // flip direction each row
@@ -110,38 +125,62 @@ void zigzag_motion_2D(Point3D start, Point3D end, FILE *fptr) {
 // main function
 
 int main() {
+
     FILE *fptr;
-    fptr = fopen("Angles.txt", "w");  
+    fptr = fopen("datafiles/Angles_radians.txt", "w");  
     if (!fptr) {
         perror("Error opening file");
         return 1;
     }
 
-    Point3D p1 = {10, 8, 0};       // 1st vertical motion
-    Point3D p2 = {10, 18, 20};
+    FILE *fptr_agnlerad;
+    fptr_agnlerad = fopen("datafiles/Angles_radians.txt", "w");  
+    if (!fptr_agnlerad) {
+        perror("Error opening file");
+        return 1;
+    }
+    FILE *fptr_angledegree;
+    fptr_angledegree = fopen("datafiles/Angles_degrees.txt", "w");  
+    if (!fptr_angledegree) {
+        perror("Error opening file");
+        return 1;
+    }
 
-    Point3D p3 = {12, 8, 0};      // horizontal motion
-    Point3D p4 = {12, 18, 20};
+    double xe,ye,ze;
+    float theta_e=0;
 
-    Point3D p5 = {15, 8, 0};      // vertical motion
-    Point3D p6 = {15, 18, 20};
+    fprintf(fptr , "xe   ye   ze  theta_0   theta_1   theta_2   theta_3\n");
+    fprintf(fptr_agnlerad , "xe   ye   ze  theta_0   theta_1   theta_2   theta_3\n");
+    fprintf(fptr_angledegree , "xe   ye   ze theta_0   theta_1   theta_2   theta_3\n");
 
-    Point3D p7 = {18, 8, 0};       //horizontal motion
-    Point3D p8 = {18, 18, 20};
+    // Point3D p1 = {10, 6.2 , 0};
+    // Point3D p2 = {10, 24, 20};
 
-    Point3D p9 = {10, 8, 0};      //vertical motion
-    Point3D p10 = {10, 18, 20};
+    Point3D p1 = {10, 6.2, 0};       // 1st vertical motion
+    Point3D p2 = {10, 20, 20};
+
+    Point3D p3 = {12, 6.2, 0};      // horizontal motion
+    Point3D p4 = {12, 20, 20};
+
+    Point3D p5 = {15, 6.2, 0};      // vertical motion
+    Point3D p6 = {15, 20, 20};
+
+    Point3D p7 = {18, 6.2, 0};       //horizontal motion
+    Point3D p8 = {18, 20, 20};
+
+    Point3D p9 = {10, 6.2, 0};      //vertical motion
+    Point3D p10 = {10, 20, 20};
 
 
-    zigzag_motion_1D(p1, p2, fptr);
-    zigzag_motion_2D(p3,p4,fptr);
-    zigzag_motion_1D(p5,p6,fptr);
-    zigzag_motion_2D(p7,p8,fptr);
+    zigzag_motion_1D(p1, p2, fptr, fptr_agnlerad,fptr_angledegree);
+    zigzag_motion_2D(p3,p4, fptr,fptr_agnlerad,fptr_angledegree);
+    zigzag_motion_1D(p5,p6,fptr,fptr_agnlerad,fptr_angledegree);
+    zigzag_motion_2D(p7,p8,fptr,fptr_agnlerad,fptr_angledegree);
 
     // zigzag_motion_2D(p9,p10,fptr);
 
-
-
     fclose(fptr);
+    fclose(fptr_agnlerad);
+    fclose(fptr_angledegree);
     return 0;
 }
--- src/SquareMapping.c
+++ src/SquareMapping.c
@@ -1,101 +1,3 @@
-// #include <stdio.h>
-
-// typedef struct {
-//     int x, y, z;
-// } Point3D;
-
-// #define NUM_POINTS 4   // change based on how many points you have
-
-// // Example path points
-// Point3D path[NUM_POINTS] = {
-//     {150, 250, -200},
-//     {-150, 250, 200},
-//     {-140, 240, 200},
-//     {140, 240, 200}
-// };
-
-// // Zigzag state
-// int direction = 1;   // 1 = forward, -1 = backward
-// int current_index = 0;
-
-// Point3D zigzag_envelope() {
-//     Point3D p = path[current_index];
-
-//     // Move index for next call
-//     current_index += direction;
-
-//     // Reverse direction at ends
-//     if (current_index >= NUM_POINTS) {
-//         direction = -1;
-//         current_index = NUM_POINTS - 2;
-//     } else if (current_index < 0) {
-//         direction = 1;
-//         current_index = 1;
-//     }
-
-//     return p;
-// }
-
-// int main() {
-//     for (int i = 0; i < 20; i++) {
-//         Point3D p = zigzag_envelope();
-//         printf("Step %d -> (%d, %d, %d)\n", i, p.x, p.y, p.z);
-//     }
-//     return 0;
-// }
-
-// 
-// #include <stdio.h>
-// 
-// typedef struct {
-    // int x, y, z;
-// } Point3D;
-// 
-// #define STEP_Y 20   // step size along Y
-// #define STEP_X 20   // step size along X
-// 
-// void zigzag_motion(Point3D start, Point3D end) {
-    // int dir = 1; // 1 = forward in Y, -1 = backward
-    // int z = start.z;
-// 
-    // for (int x = start.x; x >= end.x; x -= STEP_X) {
-        // if (dir == 1) {
-            // for (int y = start.y; y >= end.y; y -= STEP_Y) {
-                // printf("(%d, %d, %d)\n", x, y, z);
-            // }
-        // } else {
-            // for (int y = end.y; y <= start.y; y += STEP_Y) {
-                // printf("(%d, %d, %d)\n", x, y, z);
-            // }
-        // }
-        // dir *= -1; // flip direction each row
-    // }
-// 
-// 
-    // for (int i = 0; i < 3; i++) {
-        // fprintf(stdout, "%d %d %d\n", path[i].x, path[i].y, path[i].z);
-    // }
-// }
-// 
-// int main() {
-// 
-    // FILE *fptr;
-    // fptr = fopen('coordinates_values', 'w');
-    // 
-    // Point3D p1 = {150, 150, -200};
-    // Point3D p2 = {-150, -150, -200};
-    // fprintf(fptr,"xe | ye | ze | theta_e \n");
-    // zigzag_motion(p1, p2);
-// 
-    // fclose(fptr);
-    // return 0;
-// 
-// }
-// 
-
-// ======================================================
-
-
 // Step Delay: a milliseconds delay between the movement of each servo.  Allowed values from 10 to 30 msec.
 //    M1=base degrees. Allowed values from 0 to 180 degrees
 //    M2=shoulder degrees. Allowed values from 15 to 165 degrees
@@ -140,7 +42,7 @@ void zigzag_motion(Point3D start, Point3D end, FILE *fptr) {
 
 int main() {
     FILE *fptr;
-    fptr = fopen("coordinates_values.txt", "w");  // ✅ fixed quotes and extension
+    fptr = fopen("datafiles/Coordinates_values.txt", "w");  // ✅ fixed quotes and extension
     if (!fptr) {
         perror("Error opening file");
         return 1;
@@ -149,10 +51,31 @@ int main() {
     // write header
     // fprintf(fptr, "xe ye ze\n");
 
-    Point3D p1 = {10, 6.2 , 0};
+    // Point3D p1 = {10, 6.2 , 0};
+    // Point3D p2 = {10, 24, 20};
+
+    // zigzag_motion(p1, p2, fptr);
+
+    Point3D p1 = {10, 6.2, 0};       // 1st vertical motion
     Point3D p2 = {10, 24, 20};
 
-    zigzag_motion(p1, p2, fptr);
+    Point3D p3 = {12, 6.2, 0};      // horizontal motion
+    Point3D p4 = {12, 24, 20};
+
+    // Point3D p5 = {15, 6.2, 0};      // vertical motion
+    // Point3D p6 = {15, 24, 20};
+
+    Point3D p7 = {18, 6.2, 0};       //horizontal motion
+    Point3D p8 = {18, 24, 20};
+
+    // Point3D p9 = {10, 6.2, 0};      //vertical motion
+    // Point3D p10 = {10, 24, 20};
+
+
+    zigzag_motion_1D(p1, p2, fptr);
+    zigzag_motion_2D(p3,p4,fptr);
+    // zigzag_motion_1D(p5,p6,fptr);
+    zigzag_motion_2D(p7,p8,fptr);
 
     fclose(fptr);
     return 0;
